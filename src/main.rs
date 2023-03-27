cd6025d767a8b9bd9b9a65212a393f4153c307b9
Jedd Dryden <40693089+Jaffa-Cakes@users.noreply.github.com>
2023-03-27 16:59:45 +1100

    MRE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+	#[at("/")]
+	Root,
+	#[at("/search/:query")]
+	Search { query: String },
+}
+
+fn switch(route: Route) -> Html {
+	match route {
+		Route::Root => html!{
+			<Link<Route> to={Route::Search { query: "foo bar baz".into() }}>
+				{"Do search!"}
+			</Link<Route>>
+		},
+		Route::Search { query } => html!{
+			<p>{"You searched for: "}<code>{query.clone()}</code></p>
+		},
+	}
+}
+
+#[function_component]
+fn App() -> Html {
+	html!{
+		<BrowserRouter>
+			<Switch<Route> render={switch} />
+		</BrowserRouter>
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	yew::Renderer::<App>::new().render();
 }
